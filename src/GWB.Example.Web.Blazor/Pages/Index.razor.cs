88e3e48d1520eb284e630220f2e2114d127b9171
GerdB <GerdBauersfeld@Weltgewissen.de>
2022-09-05 16:57:00 +0200

    Separated code from html code into code behind class.

---
+++ src/GWB.Example.Web.Blazor/Pages/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace GWB.Example.Web.Blazor.Pages;
+
+using Application.Core.Domain.Entities;
+using Microsoft.AspNetCore.Components;
+using Services;
+
+public partial class Index
+{
+    private IEnumerable<Country> _countries = new List<Country>();
+
+    private bool _hidePosition;
+    private bool _loading;
+
+    [Inject] private CountryService CountryService { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _countries = await CountryService.GetAllAsync();
+    }
+
+    private async Task Reload()
+    {
+        _countries = await CountryService.GetAllAsync();
+    }
+}
